460091b6a0b648803c25ca37e25adb0a1cf09637
natsu <turkitututfm@gmail.com>
2019-07-03 11:40:08 +0200

    test unlocker animation

--- client/scenes/test.js
+++ client/scenes/test.js
@@ -4,16 +4,16 @@ export default {
     preload() {
         this.load.dragonbone(
             "character",
-            "assets/SwordsMan_tex.png",
-            "assets/SwordsMan_tex.json",
-            "assets/SwordsMan_ske.json"
+            "assets/test_tex.png",
+            "assets/test_tex.json",
+            "assets/test_ske.json"
         );
     },
     create() {
-        armature = this.add.armature("Swordsman", "character");
+        armature = this.add.armature("walk", "character");
         armature.scale = 0.4;
 
-        armature.animation.play("steady");
+        armature.animation.play();
 
         armature.x = this.cameras.main.centerX;
         armature.y = this.cameras.main.centerY + 200;
